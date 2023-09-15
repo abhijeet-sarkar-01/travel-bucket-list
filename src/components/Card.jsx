185bd5d35dc57d9baf57435b4d9bd187a3e26dac
Abhijeet Sarkar <abhijeet.sarkar7191@gmail.com>
2023-09-15 19:57:05 +0530

    change image url

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ export default function Card(props) {
   return(
     <div className="card">
       <div className="card-img">
-        <img src={`../public/images/${props.img}`} alt="Tokyo, Japan" title={props.loc} />
+        <img src={`./public/images/${props.img}`} alt="Tokyo, Japan" title={props.loc} />
       </div>
 
       <div className="card-desc">
